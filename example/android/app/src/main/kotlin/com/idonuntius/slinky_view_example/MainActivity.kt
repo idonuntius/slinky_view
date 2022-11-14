50d445823bab5e8fb729cd4c14ab1f5d8b939f3a
idonun <mc.natsu831@gmail.com>
2022-11-15 01:57:33 +0900

    + example

---
+++ example/android/app/src/main/kotlin/com/idonuntius/slinky_view_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.idonuntius.slinky_view_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:slinky_view/slinky_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SlinkyView example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final _controller = SlinkyController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlinkyView(
+      controller: _controller,
+      panelContents: [
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 10,
+            (context, index) {
+              return ColoredBox(
+                color: index % 2 == 0 ? Colors.grey.shade400 : Colors.grey.shade200,
+              );
+            },
+          ),
+          itemExtent: 120,
+        ),
+        const SliverFillRemaining(
+          hasScrollBody: false,
+          child: ColoredBox(color: Colors.white),
+        ),
+      ],
+      body: Container(
+        color: Colors.blue.shade100,
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
