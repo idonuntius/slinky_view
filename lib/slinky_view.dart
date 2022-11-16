6c918688310b9090c44908ca9e24a29b5d665c00
idonun <mc.natsu831@gmail.com>
2022-11-16 20:33:13 +0900

    Move SlinkyViewState to slinky_view file

--- lib/slinky_view.dart
+++ lib/slinky_view.dart
@@ -3,4 +3,4 @@ library slinky_view;
 export 'src/slinky_controller.dart';
 export 'src/slinky_panel_parameter.dart';
 export 'src/slinky_scroll_parameter.dart';
-export 'src/slinky_view.dart';
+export 'src/slinky_view.dart' show SlinkyView;
